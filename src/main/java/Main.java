453e85d80194e661d8a74c34bec3c9ad9abf8fff
igor raphael <169068315+igorraphael08@users.noreply.github.com>
2024-09-02 12:16:01 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+
+
+for( int i = 0 ; i < 5;i++) {
+System.out.println(i);
+  
+}
+
+    for( int j = 4 ; j >= 0; j--) {
+
+      System.out.println(j);
+
+    }
+
+    //for( início ; condição ; incremento) {
+   //comando 
+   // }
+    sc.close();
+
+    
+  }
+}
